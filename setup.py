1fc0770aa8028b903b5de4e56fb16bc26b2c5805
Yue1Harriet1 <dariahuangster@gmail.com>
2024-06-24 23:50:52 -0400

    change repo name

--- setup.py
+++ setup.py
@@ -1,13 +1,13 @@
 from setuptools import setup
 
 setup(
-    name='streamlit-rq',
+    name='streamlitrq',
     version='0.0.1',
-    description='streamlit-rq package from github repo to build task queue using redis queue for streamlit',
-    url='https://github.com/Yue1Harriet1/streamlit-rq.git',
+    description='streamlitrq package from github repo to build task queue using redis queue for streamlit',
+    url='https://github.com/Yue1Harriet1/streamlitrq.git',
     author='Yue Harriet Huang',
     author_email='dariahuangster@gmail.com',
     license='unlicense',
-    packages=['streamlit-rq'],
+    packages=['streamlitrq'],
     zip_safe=False
 )
